070fe36bd3175bff2ad2afd73fda27b0f00c0355
HazelNerio2027 <ortiz.hazel2027@gmail.com>
2026-07-23 11:35:44 -0600

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Fecha del evento: 21 de Noviembre de 2026 a las 18:00 hrs
-const eventDate = new Date(2026, 10, 21, 18, 0, 0).getTime();
+var eventDate = new Date(2026, 10, 21, 18, 0, 0).getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
